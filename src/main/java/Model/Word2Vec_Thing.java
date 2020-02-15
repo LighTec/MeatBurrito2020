f5f5462881f41220d59b59f435d856efb5b7da7c
LighTec <ktech117@gmail.com>
2020-02-15 16:31:11 -0700

    fixed pom & added new file

---
+++ src/main/java/Model/Word2Vec_Thing.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Word2Vec_Thing {
+
+    public Word2Vec_Thing(){
+
+    }
+
+    public void inputData(String filepath){
+
+    }
+
+    public void train(){
+
+    }
+
+    public Word2Vec returnModel(){
+
+    }
+}
