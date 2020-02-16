359fd6386b6dd5d3793d0ea45be8bff69fd96d0b
LighTec <ktech117@gmail.com>
2020-02-16 00:32:43 -0700

    stuff

--- src/main/java/Model/Train.java
+++ src/main/java/Model/Train.java
@@ -9,7 +9,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class  Train {
-    public static File train(MultiLayerNetwork networkConfig,File networkFile, int numOfEpochs, int[] sizedTFile, String[] validWord,double[][] simmilaratyMatrix ) throws IOException {
+    public static File train(MultiLayerNetwork networkConfig,File networkFile, int numOfEpochs, int[] sizedTFile, String[] validWord,double[][] simmilaratyMatrix) throws IOException {
         int perEpoch = (int)(sizedTFile.length / numOfEpochs);
 
         for (int epoch = 0; epoch<numOfEpochs;epoch++) {
@@ -17,11 +17,11 @@ public class  Train {
             INDArray inputLabels = Nd4j.zeros(1, validWord.length, perEpoch);//another oneD array that holds an offset version of the last to be used as output while training
             for (int j = 0 +(epoch * perEpoch) ; j < ((epoch + 1) * perEpoch) -1 ; j++) {
                 inputArray.putScalar(new int[]{0, sizedTFile[j], j}, 1);
-                for(int i = 0; i < simmilaratyMatrix.length; i++){
-                    inputLabels.putScalar(j + i , simmilaratyMatrix[j+1][i]);
-                }
+                //for(int i = 0; i < simmilaratyMatrix.length - 1 ; i++){
+                //    inputLabels.putScalar(new int[]{0,i+1,j},simmilaratyMatrix[j][i+1]);
+               // }
                 //inputLabels.putRow(j,Nd4j.create(simmilaratyMatrix[j+1]) );
-                //inputLabels.putScalar(new int[]{0, sizedTFile[j+1], j}, 1);
+                inputLabels.putScalar(new int[]{0, sizedTFile[j+1], j}, 1);
             }
 
             DataSet dataSet = new DataSet(inputArray, inputLabels);
