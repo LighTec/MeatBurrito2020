95bd9bc3eb1fb506616872a821dfc5a1899d4b04
George Smith <smith0064@gmail.com>
2020-02-15 20:09:27 -0700

    algoconfig changes

--- src/main/java/Model/Train.java
+++ src/main/java/Model/Train.java
@@ -9,17 +9,45 @@ import java.io.File;
 import java.io.IOException;
 
 public class  Train {
-    public static File train(MultiLayerNetwork networkConfig,File networkFile, int numOfEpochs, int[] sizedTFile) throws IOException {
+    public static File train(MultiLayerNetwork networkConfig,File networkFile, int numOfEpochs, int[] sizedTFile, String[] validWord) throws IOException {
         int perEpoch = (int)(sizedTFile.length / numOfEpochs);
 
         for (int epoch = 0; epoch<numOfEpochs;epoch++) {
-            INDArray inputArray = Nd4j.zeros(1, validKeyChars.length, perEpoch);//oneD arr that stores input data
-            INDArray inputLabels = Nd4j.zeros(1, validKeyChars.length, perEpoch);//another oneD array that holds an offset version of the last to be used as output while training
-
+            INDArray inputArray = Nd4j.zeros(1, validWord.length, perEpoch);//oneD arr that stores input data
+            INDArray inputLabels = Nd4j.zeros(1, validWord.length, perEpoch);//another oneD array that holds an offset version of the last to be used as output while training
+            for (int j = 0 +(epoch * perEpoch) ; j < ((epoch + 1) * perEpoch) -1 ; j++) {
+                inputArray.putScalar(new int[]{0, sizedTFile[j], j}, 1);
+                inputLabels.putScalar(new int[]{0, sizedTFile[j+1], j}, 1)
+            }
 
             DataSet dataSet = new DataSet(inputArray, inputLabels);
             networkConfig.fit(dataSet);
 
+            String output = "";
+
+            INDArray testInputArray = Nd4j.zeros(validWord.length);
+            int fistChar = (int) (validWord.length * Math.random());
+            testInputArray.putScalar(fistChar, 1);//creats a random vector based on validKey characters
+
+
+            for (int k = 0; k < 2000; k++) {
+
+                INDArray outputArray = networkConfig.rnnTimeStep(testInputArray);
+
+                double maxPrediction = Double.MIN_VALUE;
+                int maxPredictionIndex = -1;
+                for (int h = 0; h < validWord.length; h++) {
+                    if (maxPrediction < outputArray.getDouble(h)) {
+                        maxPrediction = outputArray.getDouble(h);
+                        maxPredictionIndex = h;
+                    }
+                }
+                output = output + validWord[maxPredictionIndex];
+                testInputArray = Nd4j.zeros(validWord.length);
+                testInputArray.putScalar(maxPredictionIndex, 1);
+
+            }
+            System.out.println((char) fistChar + output);
 
         }
 
