98bd032bc440a5e44754958d547a4b2e8996422c
George Smith <smith0064@gmail.com>
2020-02-15 20:27:26 -0700

    added a semi collin

--- src/main/java/Model/TwitterResources.java
+++ src/main/java/Model/TwitterResources.java
@@ -1,15 +1,12 @@
 package Model;
-import twitter4j.Twitter;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 
 public class TwitterResources {
     public TwitterResources() {
 
     }
 
-    public static void getTweets(String source, String outputFile) {
+    public static void getTweets(String source, String outputFile) throws TwitterException {
         Twitter twitter = TwitterFactory.getSingleton();
         QueryResult tweets = twitter.search(new Query("source:twitter4j yusukey"));
         for(int i = 0; i < tweets.getTweets().size(); i++) {
